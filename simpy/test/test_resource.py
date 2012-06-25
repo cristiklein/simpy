f1630dbc1dbef66ae089964e3de497c50731e4ce
Ontje L?nsdorf <the_com@gmx.de>
2012-06-25 15:27:57 +0200

    Added a test of the resource capacity.

--- simpy/test/test_resource.py
+++ simpy/test/test_resource.py
@@ -19,3 +19,26 @@ def test_resource():
     simulate(4, root, result)
 
     assert result == [('a', 0), ('b', 1)]
+
+
+def test_resource_capacity():
+    def root(ctx, result):
+        resource = Resource(ctx, 'res', capacity=3)
+
+        def child(ctx, name, resource, result):
+            yield resource.request()
+            result.append((name, ctx.now))
+            yield ctx.wait(1)
+            resource.release()
+
+        for id in range(9):
+            ctx.fork(child, '%d' % id, resource, result)
+        yield
+
+    result = []
+    simulate(4, root, result)
+
+    assert result == [('0', 0), ('1', 0), ('2', 0),
+            ('3', 1), ('4', 1), ('5', 1),
+            ('6', 2), ('7', 2), ('8', 2),
+    ]
