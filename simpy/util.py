97b6a4f5c2d463cf8382f9c8ea8e5383da9bc8a9
Stefan Scherfke <stefan@sofa-rockers.org>
2012-08-26 15:28:51 +0200

    Implemented the helper process to wait for all or any of multiple processes.

--- simpy/util.py
+++ simpy/util.py
@@ -8,6 +8,7 @@ This modules contains various utility functions:
   terminated.
 
 """
+from simpy.core import Interrupt
 
 
 def at(t):
@@ -74,9 +75,50 @@ def delayed(dt):
     return starter
 
 
-def wait_for_all(*procs):
-    pass
+def wait_for_all(procs):
+    """Return a process that waits for all ``procs``.
+
+    The result of the helper process will be a list with the results
+    of ``procs`` in their respective order.
+
+    Raise a :class:`ValueError` if no processes are passed.
+
+    """
+    if not procs:
+        raise ValueError('No processes were passed.')
+
+    def waiter(context):
+        results = []
+        for proc in procs:
+            results.append((yield proc))
+
+        context.exit(results)
+
+    return waiter
 
 
-def wait_for_any(*procs):
-    pass
+def wait_for_any(procs):
+    """Return a process that waits for the first of ``procs`` to finish.
+
+    The result of the helper process will be a tuple ``(finished_proc,
+    remaining_procs)``. You can pass the list of remaining procs to
+    another call to this method to wait for the next of them.
+
+    Raise a :class:`ValueError` if no processes are passed.
+
+    """
+    if not procs:
+        raise ValueError('No processes were passed.')
+
+    def waiter(context):
+        for proc in procs:
+            context.interrupt_on(proc)
+
+        try:
+            yield context.hold()
+        except Interrupt as interrupt:
+            finished_proc = interrupt.cause
+            procs.remove(finished_proc)
+            context.exit((finished_proc, procs))
+
+    return waiter
