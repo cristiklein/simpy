4ea9a29fcf8148c24dbed6f59df1d1e8b42db2f8
Stefan Scherfke <stefan@sofa-rockers.org>
2012-11-10 21:54:07 +0100

    API doc fixes and improvements.

--- simpy/__init__.py
+++ simpy/__init__.py
@@ -2,48 +2,62 @@
 The ``simpy`` module provides SimPy's end-user API. It therefore
 aggregates Simpy's various classes and methods:
 
-Core classes
-------------
 
-* :class:`~simpy.core.Environment`: SimPy's central class. It contains
+Core classes and functions
+--------------------------
+
+.. currentmodule:: simpy.core
+
+- :class:`Environment`: SimPy's central class. It contains
   the simulation's state and lets the PEMs interact with it (i.e.,
   schedule events).
-
-* :class:`~simpy.core.Interrupt`: This exception is thrown into
-  a process if it gets interrupted by another one.
+- :class:`Process`: This class represents a PEM while
+  it is executed in an environment. An instance of it is returned by
+  :meth:`Environment.start()`. It inherits
+  :class:`BaseEvent`.
+- :class:`Interrupt`: This exception is thrown into a process if it gets
+  interrupted by another one.
+- :func:`peek()`: Return the next event's time.
+- :func:`step()`: Process the next event.
+- :func:`simulate()`: Execute the simulation until a given time.
 
 
 Resources
 ---------
 
-- :class:`~simpy.resources.Resource`: Can be used by a limited number of
-  processes at a time (e.g., a gas station with a limited number of fuel
-  pumps).
+.. currentmodule:: simpy.resources
 
-- :class:`~simpy.resources.Container`: Models the production and
-  consumption of a homogeneous, undifferentiated bulk. It may either be
-  continuous (like water) or discrete (like apples).
-
-- :class:`~simpy.resources.Store`: Allows the production and consumption
-  of discrete Python objects.
+- :class:`Resource`: Can be used by a limited number of processes at a
+  time (e.g., a gas station with a limited number of fuel pumps).
+- :class:`Container`: Models the production and consumption of a
+  homogeneous, undifferentiated bulk. It may either be continuous (like
+  water) or discrete (like apples).
+- :class:`Store`: Allows the production and consumption of discrete
+  Python objects.
 
 
 Queues
 ------
 
-- :class:`~simpy.queues.FIFO`: A simple FIFO queue.
-- :class:`~simpy.queues.LIFO`: A simple LIFO queue.
-- :class:`~simpy.queues.Priority`: A :mod:`heapq`-based priority queue.
+.. currentmodule:: simpy.queues
+
+- :class:`FIFO`: A simple FIFO queue.
+- :class:`LIFO`: A simple LIFO queue.
+- :class:`Priority`: A :mod:`heapq`-based priority queue.
 
 
 Monitoring
 ----------
+
+.. currentmodule:: simpy.monitoring
 
 *[Not yet implemented]*
 
 
 Other
 -----
+
+.. currentmodule:: simpy
 
 .. autofunction:: test
 
--- simpy/core.py
+++ simpy/core.py
@@ -3,27 +3,26 @@ This module contains the implementation of SimPy's core classes. Not
 all of them are intended for direct use and are thus not importable
 directly via ``from simpy import ...``.
 
-* :class:`Environment`: SimPy's central class. It contains the
+- :class:`Environment`: SimPy's central class. It contains the
   simulation's state and lets the PEMs interact with it (i.e., schedule
   events).
-
-* :class:`~simpy.core.Process`: This class represents a PEM while
+- :class:`~simpy.core.Process`: This class represents a PEM while
   it is executed in an environment. An instance of it is returned by
-  :meth:`Environment.start()`.
-
-* :class:`Interrupt`: This exception is thrown into a process if it gets
+  :meth:`Environment.start()`. It inherits :class:`BaseEvent`.
+- :class:`Interrupt`: This exception is thrown into a process if it gets
   interrupted by another one.
 
 The following classes should not be imported directly:
 
-* :class:`BaseEvent`: Base class for all events.
-
-* :class:`Timeout`: Can be yielded by a PEM to hold its state or wait
+- :class:`BaseEvent`: Base class for all events.
+- :class:`Timeout`: Can be yielded by a PEM to hold its state or wait
   for a certain amount of time.
+- :class:`Event`: A simple event that can be used to implement things
+  like shared resources.
 
 This module also contains a few functions to simulate an
-:class:`Environment`: :func:`peek()`, :func:`step` and the shortcut
-:func:`simulate`.
+:class:`Environment`: :func:`peek()`, :func:`step()` and the shortcut
+:func:`simulate()`.
 
 """
 from heapq import heappush, heappop
@@ -50,6 +49,14 @@ class Interrupt(Exception):
     ``cause`` may be none of no cause was explicitly passed to
     :func:`Process.interrupt()`.
 
+    An interrupt has a higher priority as a normal event. Thus, if
+    a process has a normal event and an interrupt scheduled at the same
+    time, the interrupt will always be thrown into the PEM first.
+
+    If a process is interrupted multiple times at the same time, all
+    interrupts will be thrown into the PEM in the same order as they
+    occurred.
+
     """
     def __init__(self, cause):
         super(Interrupt, self).__init__(cause)
@@ -66,6 +73,19 @@ class BaseEvent(object):
 
     Every event is bound to an :class:`Environment` ``env`` and has a
     list of ``callbacks`` that are called when the event is processed.
+
+    A callback can be any callable that accepts the following arguments:
+
+    - *event:* The :class:`BaseEvent` instance the callback was
+      registered at.
+    - *success:* Boolean that indicates if the event was successful.
+      A process that raised an uncaught exception might for example
+      cause an unsuccessful (failed) event.
+    - *value:* An event can optionally send an arbitrary value. It
+      defaults to ``None``.
+
+    You can add callbacks by appending them to the ``callbacks``
+    attribute of an event.
 
     """
     __slots__ = ('callbacks', '_env', '_scheduled')
@@ -84,19 +104,58 @@ class BaseEvent(object):
 
 
 class Event(BaseEvent):
+    """A simple event that can be scheduled by calling its
+    :meth:`succeed()` or :meth:`fail()` method.
+
+    You should not instantiate this class directly, but call the factory
+    method :meth:`Environment.event()` instead.
+
+    """
     __slots__ = ('callbacks', '_env', '_scheduled')
 
     def __init__(self, env):
         super(Event, self).__init__(env)
 
     def succeed(self, value=None):
+        """Schedule the event and mark it as successful.
+
+        You can optionally pass an arbitrary ``value`` that will be sent
+        into processes waiting for that event.
+
+        Raise a :exc:`RuntimeError` if this event has already been
+        scheduled.
+
+        """
         self._env._schedule(EVT_RESUME, self, SUCCEED, value)
 
-    def fail(self, value=None):
-        self._env._schedule(EVT_RESUME, self, FAIL, value)
+    def fail(self, exception):
+        """Schedule the event and mark it as failed.
+
+        The ``exception`` will be thrown into processes waiting for that
+        event.
+
+        Raise a :exc:`ValueError` if ``exception`` is not an
+        :exc:`Exception`.
+
+        Raise a :exc:`RuntimeError` if this event has already been
+        scheduled.
+
+        """
+        if not isinstance(exception, Exception):
+            raise ValueError('%s is not an exception.' % exception)
+        self._env._schedule(EVT_RESUME, self, FAIL, exception)
 
 
 class Timeout(BaseEvent):
+    """An event that is scheduled with a certain ``delay`` after its
+    creation.
+
+    This event can be used by processes to wait (or hold their state)
+    for ``delay`` time steps. It is immediately scheduled at ``env.now
+    + delay`` and has thus (in contrast to :class:`Event`) no
+    *success()* or *fail()* method.
+
+    """
     __slots__ = ('callbacks', '_env', '_scheduled')
 
     def __init__(self, env, delay, value=None):
@@ -107,12 +166,16 @@ class Timeout(BaseEvent):
 
 
 class Process(BaseEvent):
-    """A *Process* is a wrapper for instantiated PEMs.
+    """A *Process* is a wrapper for instantiated PEMs during their
+    execution.
 
-    A Processes has a process event generator (``peg`` -- the generator
-    that the PEM returns) and a reference to its :class:`Environment`
-    ``env``. It also contains internal and external status information.
-    It is also used for process interaction, e.g., for interruptions.
+    A Processes has a generator (the generator that the PEM returns) and
+    a reference to its :class:`Environment` ``env``. It also contains
+    internal and external status information.  It is also used for
+    process interaction, e.g., for interruptions.
+
+    ``Process`` inherits :class:`BaseEvent`. You can thus wait for the
+    termination of a process by simply yielding it from your PEM.
 
     An instance of this class is returned by
     :meth:`Environment.start()`.
@@ -142,6 +205,13 @@ class Process(BaseEvent):
         return '%s(%s)' % (self.__class__.__name__, self.name)
 
     def interrupt(self, cause=None):
+        """Interupt this process optionally providing a ``cause``.
+
+        A process cannot be interrupted if it already terminated.
+        A process can also not interrupt itself. Raise
+        a :exc:`RuntimeError` in these cases.
+
+        """
         if not self.is_alive:
             raise RuntimeError('%s has terminated and cannot be interrupted.' %
                                self)
@@ -159,7 +229,14 @@ class Process(BaseEvent):
         event.callbacks.append(self._process)
         self._env._schedule(EVT_INTERRUPT, event, FAIL, Interrupt(cause))
 
-    def _process(self, event, succeed, value):
+    def _process(self, event, success, value):
+        """Get the next event from this process and register as a callback.
+
+        If the PEM generator exits or raises an exception, terminate
+        this process. Also schedule this process to notify all
+        registered callbacks, that the process terminated.
+
+        """
         # Ignore dead processes. Multiple concurrently scheduled
         # interrupts cause this situation. If the process dies while
         # handling the first one, the remaining interrupts must be
@@ -173,7 +250,7 @@ class Process(BaseEvent):
 
         # Get next event from process
         try:
-            new_event = self._generator.send(value) if succeed else \
+            new_event = self._generator.send(value) if success else \
                         self._generator.throw(value)
 
         # We should have been interrupted but already terminated.
@@ -188,20 +265,20 @@ class Process(BaseEvent):
         # The generator exited or raised an exception.
         except (StopIteration, BaseException) as err:
             if type(err) is StopIteration:
-                succeed = SUCCEED
+                success = SUCCEED
                 value = err.args[0] if len(err.args) else None
 
             else:
                 if not self.callbacks:
                     raise err
-                succeed = FAIL
+                success = FAIL
                 # FIXME Isn't there a better way to obtain the exception
                 # type? For example using (type, value, traceback)
                 # tuple?
                 value = type(err)(*err.args)
                 value.__cause__ = err
 
-            self._env._schedule(EVT_RESUME, self, succeed, value)
+            self._env._schedule(EVT_RESUME, self, success, value)
             self._env._active_proc = None
             return
 
@@ -246,7 +323,7 @@ class Environment(object):
         return self._now
 
     def start(self, generator):
-        """Start a new process for ``generator``.
+        """Start and return a new :class:`Process` for ``generator``.
 
         ``generator`` is the generator returned by a *PEM*.
 
@@ -257,16 +334,18 @@ class Environment(object):
         """Stop the current process, optionally providing a ``result``.
 
         The ``result`` is sent to processes waiting for the current
-        process and can also be obtained via :attr:`Process.result`.
+        process.
 
         """
         raise StopIteration(result)
 
     def event(self):
+        """Create and return a new :class:`Event`."""
         return Event(self)
 
     def timeout(self, delay, value=None):
-        """Schedule a new event in ``delay`` time units.
+        """Schedule (and return) a new :class:`Timeout` event for
+        ``delay`` time units.
 
         Raise a :exc:`ValueError` if ``delta_t < 0``.
 
@@ -279,9 +358,34 @@ class Environment(object):
         return Timeout(self, delay, value)
 
     def suspend(self):
+        """Return a :class:`BaseEvent`.
+
+        That event can and will not be scheduled. Yielding that event
+        will suspend the processes until it is interrupted by another
+        one.
+
+        """
         return BaseEvent(self)
 
     def _schedule(self, evt_type, event, succeed, value=None, delay=None):
+        """Schedule the given ``event`` of type ``evt_type``.
+
+        ``evt_type`` should be one of the ``EVT_*`` constants defined on
+        top of this module.
+
+        If ``succeed`` is ``True``, the optional ``value`` will be sent
+        into all processes waiting for that event.
+
+        If ``succeed`` is ``False``, the exception in ``value`` is
+        thrown into all processes waiting for that event.
+
+        The event will be scheduled at the simulation time ``self._now
+        + delay`` or at the current time if no value is provided.
+
+        Raise a :exc:`RuntimeError` if the event already has an event
+        scheduled.
+
+        """
         if event._scheduled:
             raise RuntimeError('BaseEvent %s already scheduled.' % event)
 
