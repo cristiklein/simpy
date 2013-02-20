17a829924cc1355866fa4643e9ad9e7a9431217a
Stefan Scherfke <stefan@sofa-rockers.org>
2013-02-20 09:45:43 +0100

    Added Keith?s process communication example.

---
+++ docs/examples/code/process_communication.py
@@ -0,0 +1,143 @@
+"""
+Process communication example
+
+Covers:
+
+- Resources: Store
+
+Scenario:
+  This example shows how to interconnect simulation model elements
+  together using "resources.Store" for one-to-one, and many-to-one
+  asynchronous processes. For one-to-many a simple BroadCastPipe class
+  is constructed from Store.
+
+When Useful:
+  When a consumer process does not always wait on a generating process
+  and these processes run asynchronously. This example shows how to
+  create a buffer and also tell is the consumer process was late
+  yielding to the event from a generating process.
+
+  This is also useful when some information needs to be broadcast to
+  many receiving processes
+
+  Finally, using pipes can simplify how processes are interconnected to
+  each other in a simulation model.
+
+Example By:
+  Keith Smith
+
+"""
+import random
+
+import simpy
+import simpy.util
+
+
+RANDOM_SEED = 42
+SIM_TIME = 100
+
+
+class BroadcastPipe(object):
+    """A Broadcast pipe that allows one process to send messages to many.
+
+    This construct is useful when message consumers are running at
+    different rates than message generators and provides an event
+    buffering to the consuming processes.
+
+    The parameters are used to create a new
+    :class:`~simpy.resources.Store` instance each time
+    :meth:`get_output_conn()` is called.
+
+    """
+    def __init__(self, env, capacity=simpy.core.Infinity,
+                 item_q_type=simpy.resources.queues.FIFO,
+                 event_type=simpy.resources.events.StoreEvent):
+        self.env = env
+        self.capacity = capacity
+        self.item_q_type = item_q_type
+        self.event_type = event_type
+        self.pipes = []
+
+    def put(self, value):
+        """Broadcast a *value* to all receivers."""
+        if not self.pipes:
+            raise RuntimeError('There are no output pipes.')
+        events = [store.put(value) for store in self.pipes]
+        return simpy.util.all_of(events)  # Condition event for all "events"
+
+    def get_output_conn(self):
+        """Get a new output connection for this broadcast pipe.
+
+        The return value is a :class:`~simpy.resources.Store`.
+
+        """
+        pipe = simpy.Store(self.env, capacity=self.capacity,
+                item_q_type=self.item_q_type, event_type=self.event_type)
+        self.pipes.append(pipe)
+        return pipe
+
+
+def message_generator(name, env, out_pipe):
+    """A process which randomly generates messages."""
+    while True:
+        # wait for next transmission
+        yield env.timeout(random.randint(6, 10))
+
+        # messages are time stamped to later check if the consumer was
+        # late getting them.  Note, using event.triggered to do this may
+        # result in failure due to FIFO nature of simulation yields.
+        # (i.e. if at the same env.now, message_generator puts a message
+        # in the pipe first and then message_consumer gets from pipe,
+        # the event.triggered will be True in the other order it will be
+        # False
+        msg = (env.now, '%s says hello at %d' % (name, env.now))
+        out_pipe.put(msg)
+
+
+def message_consumer(name, env, in_pipe):
+    """A process which consumes messages."""
+    while True:
+        # Get event for message pipe
+        msg = yield in_pipe.get()
+
+        if msg[0] < env.now:
+            # if message was already put into pipe, then
+            # message_consumer was late getting to it. Depending on what
+            # is being modeled this, may, or may not have some
+            # significance
+            print('LATE Getting Message: at time %d: %s received message: %s' %
+                    (env.now, name, msg[1]))
+
+        else:
+            # message_consumer is synchronized with message_generator
+            print('at time %d: %s received message: %s.' %
+                    (env.now, name, msg[1]))
+
+        # Process does some other work, which may result in missing messages
+        yield env.timeout(random.randint(4, 8))
+
+
+# Setup and start the simulation
+print('Process communication')
+random.seed(RANDOM_SEED)
+env = simpy.Environment()
+
+# For one-to-one or many-to-one type pipes, use resource.Store
+pipe = simpy.Store(env)
+env.start(message_generator('Generator A', env, pipe))
+env.start(message_consumer('Consumer A', env, pipe))
+
+print('\nOne-to-one pipe communication\n')
+simpy.simulate(env, until=SIM_TIME)
+
+# For one-to many use BroadcastPipe
+# (Note: could also be used for one-to-one,many-to-one or many-to-many)
+env = simpy.Environment()
+bc_pipe = BroadcastPipe(env)
+
+env.start(message_generator('Generator A', env, bc_pipe))
+env.start(message_consumer('Consumer A', env, bc_pipe.get_output_conn()))
+env.start(message_consumer('Consumer B', env, bc_pipe.get_output_conn()))
+
+print('\nOne-to-many pipe communication\n')
+simpy.simulate(env, until=SIM_TIME)
---
+++ docs/examples/process_communication.rst
@@ -0,0 +1,33 @@
+=====================
+Process Communication
+=====================
+
+Covers:
+
+- Process interconnects
+- Process synchronization
+
+This example shows how to interconnect simulation model elements together using
+"resources.Store" for one-to-one, and many-to-one asynchronous processes. For
+one-to-many a simple BroadCastPipe class is constructed from Store.
+
+When Useful:
+  When a consumer process does not always wait on a generating process
+  and these processes run asynchronously. This example shows how to
+  create a buffer and also tell is the consumer process was late
+  yielding to the event from a generating process.
+
+  This is also useful when some information needs to be broadcast to
+  many receiving processes
+
+  Finally, using pipes can simplify how processes are interconnected to
+  each other in a simulation model.
+
+Example By:
+  Keith Smith
+
+.. literalinclude:: code/process_communication.py
+
+The simulations output:
+
+.. literalinclude:: code/process_communication.out
