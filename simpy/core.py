7a952a281c5f6e669cf2675803fc3d3a0d4a87eb
Ontje L?nsdorf <the_com@gmx.de>
2013-02-27 22:11:49 +0100

    Also check for the existence of the ``__traceback__`` attribute.

--- simpy/core.py
+++ simpy/core.py
@@ -52,15 +52,19 @@ if sys.version_info[0] < 3:
     LEGACY_SUPPORT = True
 
     # Python 2.x does not report exception chains. To emulate the behaviour of
-    # Python 3 and the functions format_chain and print_chain are added. The
-    # latter function is used to override the exception hook of Python 2.x.
+    # Python 3 the functions format_chain and print_chain are added. The latter
+    # function is used to override the exception hook of Python 2.x.
 
     from traceback import format_exception
 
     def format_chain(exc_type, exc_value, exc_traceback):
         if hasattr(exc_value, '__cause__') and exc_value.__cause__:
-            lines = format_chain(type(exc_value.__cause__),
-                    exc_value.__cause__, exc_value.__traceback__)
+            cause = exc_value.__cause__
+            if hasattr(exc_value, '__traceback__'):
+                traceback = exc_value.__traceback__
+            else:
+                traceback = None
+            lines = format_chain(type(cause), cause, traceback)
             lines += ('\nThe above exception was the direct cause of the '
                     'following exception:\n\n')
         else:
