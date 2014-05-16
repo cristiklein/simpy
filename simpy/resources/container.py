1db0443db8602be00c187e6f1eb35e0e2908a05d
Ontje L?nsdorf <oluensdorf@gmail.com>
2014-05-16 17:14:10 +0200

    Try to improve documenation of simpy.resources.container.

--- simpy/resources/container.py
+++ simpy/resources/container.py
@@ -1,68 +1,64 @@
 """
-This module contains all :class:`Container` like resources.
+Resource for sharing homogeneous matter between processes, either continuous
+(water) or discrete (apples).
 
-Containers model the production and consumption of a homogeneous,
-undifferentiated bulk. It may either be continuous (like water) or discrete
-(like apples).
-
-For example, a gasoline station stores gas (petrol) in large tanks.  Tankers
-increase, and refuelled cars decrease, the amount of gas in the station's
-storage tanks.
-
+A :class:`Container` can be used to model the fuel tank of a gasoline station.
+Tankers increase and refuelled cars decrease the amount of gas in the station's
+fuel tanks.
 """
+
 from simpy.core import BoundClass
 from simpy.resources import base
 
 
 class ContainerPut(base.Put):
-    """An event that puts *amount* into the *container*. The event is triggered
-    as soon as there's enough space in the *container*.
+    """Request to put *amount* of matter into the *container*. The request will
+    be triggered once there is enough space in the *container* available.
 
     Raise a :exc:`ValueError` if ``amount <= 0``.
-
     """
+
     def __init__(self, container, amount):
         if amount <= 0:
             raise ValueError('amount(=%s) must be > 0.' % amount)
-        #: The amount to be put into the container.
         self.amount = amount
-        """The amount to be put into the container."""
+        """The amount of matter to be put into the container."""
 
         super(ContainerPut, self).__init__(container)
 
 
 class ContainerGet(base.Get):
-    """An event that gets *amount* from the *container*. The event is triggered
-    as soon as there's enough content available in the *container*.
+    """Request to get *amount* of matter from the *container*. The request will
+    be triggered once there is enough matter availabe in the *container*.
 
     Raise a :exc:`ValueError` if ``amount <= 0``.
-
     """
+
     def __init__(self, resource, amount):
         if amount <= 0:
             raise ValueError('amount(=%s) must be > 0.' % amount)
         self.amount = amount
-        """The amount to be taken out of the container."""
+        """The amount of matter to be taken out of the container."""
 
         super(ContainerGet, self).__init__(resource)
 
 
 class Container(base.BaseResource):
-    """Models the production and consumption of a homogeneous, undifferentiated
-    bulk. It may either be continuous (like water) or discrete (like apples).
+    """Resource containing up to *capacity* of matter, supporting requests to
+    :meth:`put` or :meth:`get` matter into/from the container. It may either be
+    continuous (like water) or discrete (like apples).
 
     The *env* parameter is the :class:`~simpy.core.Environment` instance the
     container is bound to.
 
-    The *capacity* defines the size of the container and must be a positive
-    number (> 0). By default, a container is of unlimited size.  You can
-    specify the initial level of the container via *init*. It must be >=
-    0 and is 0 by default.
+    The *capacity* defines the size of the container. By default, a container
+    is of unlimited size. The initial amount of matter is specified by *init*
+    and defautls to ``0``.
 
     Raise a :exc:`ValueError` if ``capacity <= 0``, ``init < 0`` or
     ``init > capacity``.
-
     """
+
     def __init__(self, env, capacity=float('inf'), init=0):
         super(Container, self).__init__(env)
         if capacity <= 0:
@@ -82,17 +78,15 @@ class Container(base.BaseResource):
 
     @property
     def level(self):
-        """The current level of the container (a number between ``0`` and
-        ``capacity``).
+        """The current amount of the matter in the container."""
 
-        """
         return self._level
 
     put = BoundClass(ContainerPut)
-    """Creates a new :class:`ContainerPut` event."""
+    """Request to put *amount* of matter into the container."""
 
     get = BoundClass(ContainerGet)
-    """Creates a new :class:`ContainerGet` event."""
+    """Request to get *amount* of matter out of the container."""
 
     def _do_put(self, event):
         if self._capacity - self._level >= event.amount:
