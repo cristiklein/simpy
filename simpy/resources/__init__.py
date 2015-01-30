23a3b1a3afa79f0384e26e3bd6384200e51d399c
Stefan Scherfke <stefan@sofa-rockers.org>
2015-01-30 09:42:50 +0100

    A few minor improvements for the docs and tox.

--- simpy/resources/__init__.py
+++ simpy/resources/__init__.py
@@ -2,11 +2,13 @@
 SimPy implements three types of resources that can be used to synchronize
 processes or to model congestion points:
 
+.. currentmodule:: simpy.resources
+
 .. autosummary::
 
-    ~simpy.resources.resource
-    ~simpy.resources.container
-    ~simpy.resources.store
+   resource
+   container
+   store
 
 They are derived from the base classes defined in the
 :mod:`~simpy.resources.base` module. These classes are also meant to support
